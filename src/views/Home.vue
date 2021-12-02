59c06c8f607bfc8641c3c74588ec4c961a7cf886
godprogrammer3 <61010103@kmitl.ac.th>
2021-12-03 02:06:30 +0700

    finished test add locker

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Regis Locker Test</h1>
+    <h1>Name : {{ name }}</h1>
+    <h1>Locker UID: {{ lockerUid }}</h1>
+    <QRCode :text="lockerUid"></QRCode>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import VueQRCodeComponent from "vue-qrcode-component";
+import io from "socket.io-client";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    QRCode: VueQRCodeComponent,
+  },
+  data() {
+    return {
+      lockerUid: process.env.VUE_APP_LOCKER_UID,
+      socket: null,
+      lockerName: null,
+    };
+  },
+  mounted() {
+    this.socket = io(
+      `${process.env.VUE_APP_SOCKET_URL}:${process.env.VUE_APP_SOCKET_PORT}/${process.env.VUE_APP_SOCKET_NAME_SPACE}`
+    );
+    this.socket.on("connect", () => {
+      console.log("connected");
+    });
+    this.socket.on("disconnect", () => {
+      console.log("disconnected");
+    });
+    this.socket.on("connect_error", (error) => {
+      console.log("connect_error:", error);
+    });
+    this.socket.on("locker_update", (lockerData) => {
+      console.log("locker_update:", lockerData);
+      this.lockerName = lockerData.name;
+    });
+  },
+  computed: {
+    name() {
+      return this.lockerName ?? "Not Already Registered";
+    },
+  },
+};
 </script>
+
+<style scoped>
+.home {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
